1da191b85819fc415d0610d79848faaabb294241
robertito1 <orazurobert@yahoo.com>
2020-08-05 20:10:45 +0100

    frontend

---
+++ frontend/src/components/Login/Login.component.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+// A SIMPLE COMPONENT THAT REQUESTS USERNAME WHEN A EVERY NEW CONNECTION IS MADE
+class Login extends Component {
+
+    // THIS FUNCTION TAKES THE USERNAME INPUT VALUE AND MAKES IT AVAILABLE IN THE APP COMPONENT USING PROPS
+    login = (e) => {
+        e.preventDefault()
+        this.props.setUsername(e.target.username.value)
+    }
+    render() {
+        return (
+            <div className="login">
+                <form onSubmit={this.login}>
+                    <label>Username:</label>
+                    <input type='text' id='username' />
+                    <input type='submit' value='Log in' />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
