22a8892eedeac4336fd0750df210915e02b6fefc
robertito1 <orazurobert@yahoo.com>
2020-08-06 18:25:48 +0100

    minor fixes

--- server/server.js
+++ server/server.js
@@ -8,29 +8,29 @@ let clients = [];
 
 // FUNCTION TO PUSH EVERY NEW CONNECTION INTO THE ARRAY AS A CLIENT
 wss.on("connection", (connection) => {
-  clients.push(connection);
-  broadcast({username="admin", message: "A User Has Joined The Chat" });
+    clients.push(connection);
+    broadcast({ username: "admin", message: "A User Has Joined The Chat" });
 
-  // EVERY MESSAGE RECIEVED BY THE SERVER IS PUBLISHED TO ALL CLIENTS
-  connection.on("message", (message) => {
-    const data = JSON.parse(message);
-    broadcast(data);
-  });
+    // EVERY MESSAGE RECIEVED BY THE SERVER IS PUBLISHED TO ALL CLIENTS
+    connection.on("message", (message) => {
+        const data = JSON.parse(message);
+        broadcast(data);
+    });
 });
 
 setInterval(cleanUp, 100);
 
 function broadcast(message) {
-  const data = JSON.stringify(message);
-  clients.forEach((client) => client.send(data));
+    const data = JSON.stringify(message);
+    clients.forEach((client) => client.send(data));
 }
 
 function cleanUp() {
-  const clientsLeaving = clients.filter(
-    (client) => client.readyState === client.CLOSED
-  );
-  clients = clients.filter((client) => client.readyState !== client.CLOSED);
-  clientsLeaving.forEach((client) =>
-    broadcast({username="admin", message: "A User Has Left The Chat" })
-  );
+    const clientsLeaving = clients.filter(
+        (client) => client.readyState === client.CLOSED
+    );
+    clients = clients.filter((client) => client.readyState !== client.CLOSED);
+    clientsLeaving.forEach((client) =>
+        broadcast({ username: "admin", message: "A User Has Left The Chat" })
+    );
 }
